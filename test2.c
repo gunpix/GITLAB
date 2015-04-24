936eb68f71fbffbb7464422369e259001a353759
Supavit130 <supavit130@gmail.com>
2015-04-24 19:22:17 +0700

    modify test2 and add test3

--- test2.c
+++ test2.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main()
 {
-	for(int i=0;i<100;i++)
-	printf("testkrub");
+	for(int i=1;i<100;i++)
+	printf(“test naaa”);
 
 	return 0;
 }
---
+++ test3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	for(int i=1;i<199;i++)
+	printf(“test naaa1”);
+	printf(“test naaa2”);
+
+	return 0;
+}
